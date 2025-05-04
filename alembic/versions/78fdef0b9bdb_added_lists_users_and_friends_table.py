3b69b10ad8241f30a6a73dbc2d3d6ff687024f5a
Shonueld <seangriffindev@gmail.com>
2025-05-04 16:59:00 -0700

    fix table order

--- alembic/versions/78fdef0b9bdb_added_lists_users_and_friends_table.py
+++ alembic/versions/78fdef0b9bdb_added_lists_users_and_friends_table.py
@@ -10,7 +10,6 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision: str = '78fdef0b9bdb'
 down_revision: Union[str, None] = '2399c996cf07'
@@ -20,6 +19,13 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     op.create_table(
+        "users",
+        sa.Column("user_id", sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column("username", sa.String, nullable=False, unique=True),
+        sa.Column("created_at", sa.DateTime(timezone=True), server_default=sa.func.now(), nullable=False),
+    )
+
+    op.create_table(
         "lists",
         sa.Column("list_id", sa.Integer, primary_key=True, autoincrement=True),
         sa.Column("user_id", sa.Integer, sa.ForeignKey("users.user_id"), nullable=False),
@@ -27,12 +33,7 @@ def upgrade() -> None:
         sa.Column("status", sa.String, nullable=False),  # e.g. "completed", "wishlist", "in progress"
         sa.Column("created_at", sa.DateTime(timezone=True), server_default=sa.func.now(), nullable=False),
     )
-    op.create_table(
-        "users",
-        sa.Column("user_id", sa.Integer, primary_key=True, autoincrement=True),
-        sa.Column("username", sa.String, nullable=False, unique=True),
-        sa.Column("created_at", sa.DateTime(timezone=True), server_default=sa.func.now(), nullable=False),
-    )
+
     op.create_table(
         "friends",
         sa.Column("user_id", sa.Integer, sa.ForeignKey("users.user_id"), primary_key=True),
@@ -40,21 +41,23 @@ def upgrade() -> None:
         sa.Column("created_at", sa.DateTime(timezone=True), server_default=sa.func.now(), nullable=False),
         sa.CheckConstraint("user_id != friend_id", name="no_self_friendship")
     )
+
     op.create_table(
-        'reviews',
-        sa.Column('review_id', sa.Integer, primary_key=True, autoincrement=True),
-        sa.Column('set_id', sa.Integer, sa.ForeignKey('sets.id', ondelete='CASCADE'), nullable=False),
-        sa.Column('user_id', sa.Integer, sa.ForeignKey('users.user_id', ondelete='CASCADE'), nullable=False),
-        sa.Column('rating', sa.Integer, nullable=False),
-        sa.Column('description', sa.String, nullable=False),
+        "reviews",
+        sa.Column("review_id", sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column("set_id", sa.Integer, sa.ForeignKey("sets.id", ondelete="CASCADE"), nullable=False),
+        sa.Column("user_id", sa.Integer, sa.ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False),
+        sa.Column("rating", sa.Integer, nullable=False),
+        sa.Column("description", sa.String, nullable=False),
         sa.Column("created_at", sa.DateTime(timezone=True), server_default=sa.func.now(), nullable=False),
     )
+
     op.create_table(
-        'issues',
-        sa.Column('issue_id', sa.Integer, primary_key=True, autoincrement=True),
-        sa.Column('set_id', sa.Integer, sa.ForeignKey('sets.id', ondelete='CASCADE'), nullable=False),
-        sa.Column('user_id', sa.Integer, sa.ForeignKey('users.user_id', ondelete='CASCADE'), nullable=False),
-        sa.Column('description', sa.String, nullable=False),
+        "issues",
+        sa.Column("issue_id", sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column("set_id", sa.Integer, sa.ForeignKey("sets.id", ondelete="CASCADE"), nullable=False),
+        sa.Column("user_id", sa.Integer, sa.ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False),
+        sa.Column("description", sa.String, nullable=False),
         sa.Column("created_at", sa.DateTime(timezone=True), server_default=sa.func.now(), nullable=False),
     )
 
